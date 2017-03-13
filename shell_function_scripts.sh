bbd6ebb3c10ceeef4d876665d84ae5afe9ad1039
zhangyazhou121 <zhangyazhou@aragoncs.com>
2017-03-13 14:05:27 +0800

    Update shell_function_scripts.sh

--- shell_function_scripts.sh
+++ shell_function_scripts.sh
@@ -1,4 +1,4 @@
-# 批量删除mysql数据库中表名相似的表
+# 批量删除wordpress mysql数据库中的数据表
 for((i=2;i<=17;i++));do echo "show tables like '%wp_"$i"_%'"| mysql -h localhost -u multisite3 -p multisite3 | sed '1d' >> tablelist; done
 echo "show tables like '%wp_41_%'"| mysql -h localhost -u multisite3 -p multisite3 | sed '1d' >> tablelist1
 for i in `cat tablelist1`; do echo "drop table "$i"" | mysql -h localhost -u multisite3 -p multisite3; done
